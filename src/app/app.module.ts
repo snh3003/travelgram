064298ceda561a65ae4ed40fc89975d4c67e5acc
snh3003 <shahsama542@gmail.com>
2022-02-12 14:12:31 +0530

    added req components services and pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,28 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { PostsComponent } from './components/posts/posts.component';
+import { UsersComponent } from './components/users/users.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AddpostComponent } from './pages/addpost/addpost.component';
+import { PagenotfoundComponent } from './pages/pagenotfound/pagenotfound.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    UsersComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AddpostComponent,
+    PagenotfoundComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
