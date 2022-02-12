81644e7d2b8d3954a33e666460bbf99da2640507
snh3003 <shahsama542@gmail.com>
2022-02-12 13:59:52 +0530

    req packages added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
+  firebase: {
+    projectId: 'travelgram22-81d69',
+    appId: '1:863180324873:web:f7fa6da4c57af26fecb8d8',
+    databaseURL: 'https://travelgram22-81d69-default-rtdb.firebaseio.com',
+    storageBucket: 'travelgram22-81d69.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBk564kTFgtDjtEZtIItnd-_DsBo07OCaY',
+    authDomain: 'travelgram22-81d69.firebaseapp.com',
+    messagingSenderId: '863180324873',
+    measurementId: 'G-8F53NSELL1',
+  },
   production: true
 };
