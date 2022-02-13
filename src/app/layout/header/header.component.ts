ceb632bc945c973a613577a1ea70b4981b8d2736
snh3003 <shahsama542@gmail.com>
2022-02-13 13:10:37 +0530

    header html added!!!

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  email: any;
 
-  ngOnInit(): void {
+  constructor(
+    private auth: AuthService,
+    private toastr: ToastrService,
+    private router: Router
+  ) {
+    this.auth.getUser().subscribe((user) => {
+      this.email = user?.email;
+    });
   }
 
+  ngOnInit(): void {}
+
+  async handleSignOut() {
+    try {
+      await this.auth.signOut();
+      //this.router.navigateByUrl(['/']);
+      this.toastr.success('Logged out successfully');
+    }catch(e: any) {
+      this.toastr.error(e.message);
+    }
+  }
 }
